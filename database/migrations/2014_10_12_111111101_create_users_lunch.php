6a96d314a82ab73387f83ced782b032692447b6b
Sohag Mollik <sohagmollik871@gmail.com>
2023-08-08 11:42:51 +0600

    Add files via upload

---
+++ database/migrations/2014_10_12_111111101_create_users_lunch.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+{
+    Schema::create('lunch', function (Blueprint $table) {
+        $table->integer('food_id');
+        $table->integer('u_id');
+    });
+    
+
+}
+
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+    }
+};
